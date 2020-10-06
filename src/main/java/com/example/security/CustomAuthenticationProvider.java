c108d2780d3caba381da63cc356f812d122f1e97
Meera Subbarao <meerabs@yahoo.com>
2020-10-06 16:18:17 -0400

    Update CustomAuthenticationProvider.java

--- src/main/java/com/example/security/CustomAuthenticationProvider.java
+++ src/main/java/com/example/security/CustomAuthenticationProvider.java
@@ -40,6 +40,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
 		String username = authentication.getName();
 		String password = (String) authentication.getCredentials();
+		System.out.println("Password" + password);
 
 		List<Account> listAccounts = new ArrayList<>();
 		try {
