724e0b6dfebe53de2ee9f4f45d8ae28093eba436
Meera Subbarao <meerabs@yahoo.com>
2020-10-12 10:53:54 -0400

    Update ActivityDao.java

--- src/main/java/com/example/dao/ActivityDao.java
+++ src/main/java/com/example/dao/ActivityDao.java
@@ -8,6 +8,8 @@ import com.example.bean.Transaction;
 public interface ActivityDao {
 
 	List<Transaction> findTransactionsByCashAccountNumber(String number);
+	
+	List<Transaction> findTransactionsByCustomerName(String customerName);
 
 	void insertNewActivity(Date date, String description, String number, double amount, double availablebalance);
 
