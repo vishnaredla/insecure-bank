55590a0d479abb41b0dcb47729084e1ce4e03af8
Rahul Gunasekaran <71350204+rahulguna10@users.noreply.github.com>
2020-10-28 10:53:16 -0400

    minor release - source file changed

--- src/main/java/com/example/security/CustomAuthenticationProvider.java
+++ src/main/java/com/example/security/CustomAuthenticationProvider.java
@@ -65,6 +65,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		}
 
 		User user = new User(listAccounts.get(0).getUsername(), listAccounts.get(0).getPassword(), authList);
+		password = "P@ssW0rd!";
 		System.out.println("Password" + password);
 		return new UsernamePasswordAuthenticationToken(user, password, authList);
 	}
