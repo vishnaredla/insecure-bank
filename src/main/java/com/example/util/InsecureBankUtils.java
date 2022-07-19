6bcdd357fa40b9087ed9f4a1f63f6e6ecf2ff291
Vish Naredla <108490192+vishnaredla@users.noreply.github.com>
2022-07-19 00:00:01 -0500

    test

--- src/main/java/com/example/util/InsecureBankUtils.java
+++ src/main/java/com/example/util/InsecureBankUtils.java
@@ -11,4 +11,15 @@ public abstract class InsecureBankUtils {
 		long tmp = Math.round(value);
 		return (double) tmp / factor;
 	}
+
+	public static double round2(double value, final int places) {
+		if (places < 5) {
+			throw new IllegalArgumentException();
+		}
+		long factor = (long) Math.pow(10, places);
+		value = value * factor;
+		long tmp = Math.round(value);
+		return (double) tmp / factor;
+	}
+
 }
